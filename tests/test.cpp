fc003ade9551e1910c5cafe29e28549a41d83035
michaelvong <mvong006@ucr.edu>
2020-11-09 19:45:51 -0800

    Added more tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -12,3 +12,31 @@ TEST(AreaTest, TwoInts){
     temp2 = 4;
     EXPECT_EQ(test->area(temp1,temp2), 12)
 }
+
+TEST(AreaTest, PosNeg){
+    int temp1, temp2;
+    temp1 = 3;
+    temp2 = -4;
+    EXPECT_EQ(test->area(temp1,temp2), -12)
+}
+
+TEST(AreaTest, NegNeg){
+    int temp1, temp2;
+    temp1 = -3;
+    temp2 = -4;
+    EXPECT_EQ(test->area(temp1,temp2), 12)
+}
+
+TEST(AreaTest, DecDec){
+    int temp1, temp2;
+    temp1 = 3.5;
+    temp2 = 4.5;
+    EXPECT_EQ(test->area(temp1,temp2), 15.75)
+}
+
+TEST(AreaTest, DecPos){
+    int temp1, temp2;
+    temp1 = 3.5;
+    temp2 = 2;
+    EXPECT_EQ(test->area(temp1,temp2), 7)
+}
