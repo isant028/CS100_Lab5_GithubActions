94b7c0da723f490269c8ef941cb339e2aa4e3dac
michaelvong <mvong006@ucr.edu>
2020-11-09 19:30:38 -0800

    Updated rectangle.cpp

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -10,6 +10,14 @@ double Rectangle::perimeter(){
     return (this->width * 2) + (this->height * 2)
 }
 
+void Rectangle::set_width(int w) {
+	this->width = w;
+}
+
+void Rectangle::set_height(int h){
+	this->height = h;
+}
+
 int Rectangle::area(){
 	return this->width * this->height;
 }
